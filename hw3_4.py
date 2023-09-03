1b1f9f4da472244b9734eb62b2b05abeedd9544b
Andrew-Rom <1811mail@gmail.com>
2023-09-03 18:25:13 +0300

    HW 3-4 completed + additional task

--- hw3_4.py
+++ hw3_4.py
@@ -5,8 +5,9 @@ HW 3-4
 Достаточно вернуть один допустимый вариант.
 *Верните все возможные варианты комплектации рюкзака.
 """
+import random
 
-bug_capacity = 14
+max_capacity = 14
 
 things = {'water': 4,
           'tent': 9,
@@ -22,13 +23,43 @@ things = {'water': 4,
           'camera': 3,
           'radio': 4}
 
-bug = dict()
-bug_weight = 0
 
-things_for_packing = things.copy()
+bug_versions = []
 
-for key, value in things.items():
-    if (bug_weight + value) <= bug_capacity and key in things_for_packing.keys():
-        bug.update({key: things_for_packing.pop(key)})
-        bug_weight += value
-print(bug)
+repeat = 0
+
+while 0 <= repeat <= 100000:
+
+    bug = set()
+    bug_weight = 0
+
+    while bug_weight <= max_capacity:
+
+        things_for_packing = []
+        for key in things.keys():
+            if things.get(key) < (max_capacity - bug_weight) and key not in bug:
+                things_for_packing.append(key)
+
+        if len(things_for_packing) > 0:
+            thing = random.choice(things_for_packing)
+            if (bug_weight + things.get(thing)) <= max_capacity:
+                bug.add(thing)
+                bug_weight += things.get(thing)
+        else:
+            break
+
+    if len(bug_versions) > 0:
+        unique_kit = True
+        while unique_kit:
+            for bug_version in bug_versions:
+                if len(bug - bug_version) == 0:
+                    unique_kit = False
+                    repeat += 1
+                    break
+            break
+        if unique_kit:
+            bug_versions.append(bug)
+    else:
+        bug_versions.append(bug)
+
+print(*bug_versions, sep='\n')
